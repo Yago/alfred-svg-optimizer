8c2cb9e1d08f449906fb125114072ff03d00503e
Yann Gouffon <github@yago-art.ch>
2018-07-16 08:16:49 +0200

    add first batch

---
+++ index.js
@@ -0,0 +1,53 @@
+const alfy = require('alfy');
+const SVGO = require('svgo');
+
+const options = [
+  'removeDoctype',
+  'removeXMLProcInst',
+  'removeComments',
+  'removeMetadata',
+  'removeXMLNS',
+  'removeEditorsNSData',
+  'cleanupAttrs',
+  'inlineStyles',
+  'minifyStyles',
+  'convertStyleToAttrs',
+  'cleanupIDs',
+  'removeRasterImages',
+  'removeUselessDefs',
+  'cleanupNumericValues',
+  'cleanupListOfValues',
+  'convertColors',
+  'removeUnknownsAndDefaults',
+  'removeNonInheritableGroupAttrs',
+  'removeUselessStrokeAndFill',
+  'removeViewBox',
+  'cleanupEnableBackground',
+  'removeHiddenElems',
+  'removeEmptyText',
+  'convertShapeToPath',
+  'moveElemsAttrsToGroup',
+  'moveGroupAttrsToElems',
+  'collapseGroups',
+  'convertPathData',
+  'convertTransform',
+  'removeEmptyAttrs',
+  'removeEmptyContainers',
+  'mergePaths',
+  'removeUnusedNS',
+  'sortAttrs',
+  'removeTitle',
+  'removeDesc',
+  'removeDimensions',
+  'removeStyleElement',
+  'removeScriptElement',
+];
+
+const plugins = options.map(option => ({
+  [option]: process.env[option] == 1 ? true : false,
+}));
+
+const svgo = new SVGO({ plugins });
+svgo.optimize(alfy.input, {}).then(res => {
+  alfy.output(res.data);
+});
